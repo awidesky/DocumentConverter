d31e41ea9cb7b2fd7503f91056af87f7b76fe494
Eugene Hong <awidesky@naver.com>
2024-09-25 15:05:29 +0900

    use JarPath for finding icon(causes error because uri is rsrc:./)

--- src/main/java/io/github/awidesky/documentConverter/MainFrame.java
+++ src/main/java/io/github/awidesky/documentConverter/MainFrame.java
@@ -11,7 +11,6 @@ import java.awt.Window;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -36,6 +35,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import io.github.awidesky.documentConverter.jodConverter.JodConvertManager;
 import io.github.awidesky.guiUtil.SwingDialogs;
+import io.github.awidesky.projectPath.JarPath;
 
 public class MainFrame extends JFrame {
 
@@ -61,12 +61,10 @@ public class MainFrame extends JFrame {
 	public void init() {
 		setTitle("DocumentConverter " + Main.VERSION);
 		try {
-			InputStream is = MainFrame.class.getResourceAsStream("/icon/icon.png");
-			if(is != null) {
-				BufferedImage ICON = ImageIO.read(is);
-				setIconImage(ICON);
-				if(Taskbar.isTaskbarSupported()) Taskbar.getTaskbar().setIconImage(ICON);
-			}
+			System.out.println(JarPath.getProjectPath(MainFrame.class));
+			BufferedImage ICON = ImageIO.read(new File(JarPath.getProjectPath(MainFrame.class) + "/icon/icon.png"));
+			setIconImage(ICON);
+			if(Taskbar.isTaskbarSupported()) Taskbar.getTaskbar().setIconImage(ICON);
 		} catch (IOException e) {
 			SwingDialogs.error("Unable to find icon.png", "%e%", e, false);
 			e.printStackTrace();
